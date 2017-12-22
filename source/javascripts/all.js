8831a5434fb48138c0580d6851411576db3daf59
Alban <alban.moreon@gmail.com>
2017-12-22 14:13:29 +0100

    website update

---
+++ source/javascripts/all.js
@@ -0,0 +1,38 @@
+//= require jquery
+//= require bootstrap-sprockets
+//= require_tree .
+
+$(document).ready(function(){
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.scrollReveal');
+
+});
+
+
+jQuery(document).ready(function() {
+    jQuery('.tabs .tab-links a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+
+        // Show/Hide Tabs
+        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
+    });
+});
+
